a6abe4c796a39b1f85d1a26d0aa355a19e78ab48
Clover Chapman <clo@cloverseeds.co.uk>
2018-08-04 21:22:42 +0100

    updates _tests_

--- __tests__/app.test.jsx
+++ __tests__/app.test.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 
 import App from '../src/App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
 it('it renders a title of NASA Search', () => {
   const wrapper = shallow((
